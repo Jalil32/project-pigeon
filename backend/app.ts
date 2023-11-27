6c7919cda9168a6e9e5e0829c847595cf9ee2dd9
Jalil <jalil.i.h@hotmail.com>
2023-11-27 14:37:40 +0800

    Just pushing so I can access on PC

--- backend/app.ts
+++ backend/app.ts
@@ -1,58 +1,214 @@
-import express from 'express'
+import express from 'express';
 import { Express, Request, Response } from 'express';
-import fs from 'fs';
-import { nanoid } from 'nanoid'
+import path from 'path';
+import dotenv from 'dotenv';
+import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
 
-const path = require('path');
-
-
-
+// 1) APP CONFIGURATION
+dotenv.config({ path: 'config.env' });
+const port: string = process.env.PORT ?? '3000';
+const uri: string = process.env.URI ?? '';
 const app: Express = express();
+
+mongoose
+    .connect(uri, { dbName: 'project-pigeon' })
+    .then(() => {
+        console.log('Successfully connected to database\n');
+    })
+    .catch((err) => {
+        console.log('Failed to connect to database: ' + err);
+    });
+
+// 2) MIDDLEWARE
+app.use(express.static(path.join(__dirname, 'my-app', 'build')));
 app.use(express.json());
 
-const port: number = 3000;
-const groups = JSON.parse(fs.readFileSync(`${__dirname}/mock-database/groups.json`, 'utf-8'));
-app.use(express.static(path.join(__dirname, 'my-app', 'build')));
-// let the react app to handle any unknown routes 
-// serve up the index.html if express does'nt recognize the route
+// 3) SCHEMAS
+// User Schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide your name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    passwordConfirm: {
+        type: String,
+        required: true,
+    },
+    groups: [{ type: Schema.Types.ObjectId, ref: 'Groups' }],
+});
 
-// Server react app
+// Group Schema
+const groupSchema = new mongoose.Schema({
+    name: { type: String, requied: true },
+    creator: { type: Schema.Types.ObjectId, ref: 'Users' },
+    members: [{ type: Schema.Types.ObjectId, ref: 'Users' }],
+    messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }],
+});
+
+// Message Schema
+const messageSchema = new mongoose.Schema({
+    sentFrom: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
+    content: { type: String, required: true },
+    timestamp: { type: Date, default: Date.UTC },
+    recipient: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        refPath: 'onModel',
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Groups', 'Users'],
+    },
+});
+
+// 4) MODELS
+const Users = mongoose.model('Users', userSchema);
+const Groups = mongoose.model('Groups', groupSchema);
+const Messages = mongoose.model('Messages', messageSchema);
+
+// 5) ROUTES
+// Serve React App
 app.get('/', (req: Request, res: Response) => {
-//  res.status(200).sendFile("../client/index.html");
-  res.sendFile(`${__dirname}/my-app/build/index.html`); 
-})
-// Create group
-app.post('/group', (req: Request, res: Response) => {
-  // TODO: Need to add a way to authenticate the react app so that this end point can only be used by our React App
-  // TODO: Add Error Checking
-  let UGID: string = nanoid()
+    //  res.status(200).sendFile("../client/index.html");
+    res.sendFile(`${__dirname}/my-app/build/index.html`);
+});
 
-  let newGroup =  JSON.parse(`{
-    "chatId": "${UGID}",
-    "chatName": "${req.body.groupName}",
-    "participants": [],
-    "messages": [],
-    "createdAt": "${req.body.createdAt}",
-    "createdBy": "${req.body.createdBy}"
-  }`)   
+// Group Routes
+// Get a users groups
+app.get('/group:userId', (req: Request, res: Response) => {});
 
-  newGroup.participants = req.body.participants
-  groups.push(newGroup)
-  fs.writeFileSync(`${__dirname}/mock-database/groups.json`, JSON.stringify(groups), 'utf-8')
+// Create new group
+app.post('/group', async (req: Request, res: Response) => {
+    try {
+        // Create group
+        const group = await Groups.create(req.body); // Assign group to users for (let member in group.members) { console.log(member);
 
-  console.log(UGID)
-  res.status(200).json({
-    status: "success",
-    data: newGroup
-  })
-})
-
-// Add group members
+        for (let i in group.members) {
+            console.log(group.members[i]);
+        }
 
 
-// Send message
+    } catch (err) {
+        console.log('Group could not be created: ' + err);
+        res.status(404).send({
+            status: 'Failure. Group could not be created',
+        });
+    }
+});
 
+// Add user to group
+app.post('/group:userId', (req: Request, res: Response) => {});
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+// Delete a group
+app.delete('/group:groupId', (req: Request, res: Response) => {});
+
+// User Routes
+// Get all users
+app.get('/user', async (req: Request, res: Response) => {
+    try {
+        const users = await Users.find();
+        console.log(users);
+        res.status(200).send({
+            status: 'success',
+            data: users,
+        });
+    } catch (err) {
+        console.log('Could not get users: ' + err);
+        res.status(404).send({
+            status: 'failure',
+        });
+    }
+});
+// get user
+app.get('/user/:id', async (req: Request, res: Response) => {
+    try {
+        console.log(req.params.id);
+        const user = await Users.findById(req.params.id);
+        console.log(user);
+        res.status(200).send({
+            status: 'success',
+            user: user,
+        });
+    } catch (err) {
+        console.log('Could not find user: ' + err);
+        res.status(404).send({
+            status: 'failure',
+        });
+    }
+});
+// Create a user
+app.post('/user', async (req: Request, res: Response) => {
+    try {
+        await Users.create(req.body);
+        res.status(201).send({
+            status: 'User successfully created',
+        });
+    } catch (err: any) {
+        if (err.code === 11000) {
+            res.status(409).send({
+                status: 'An account already exists. Please login',
+            });
+        } else {
+            res.status(400).send({
+                status: 'Sorry, an account could not be created at this time',
+            });
+        }
+    }
+});
+
+// Delete a user
+app.delete('/user/:id', async (req: Request, res: Response) => {
+    try {
+        const user = await Users.findByIdAndDelete(req.params.id);
+        res.status(200).send({
+            status: 'User successfully deleted',
+            data: user,
+        });
+    } catch (err) {
+        console.log('User could not be deleted: ' + err);
+        res.status(404).send({
+            status: 'Failure. User could not be deleted',
+        });
+    }
+});
+
+// Update a user
+app.patch('/user/:id', async (req: Request, res: Response) => {
+    try {
+        const user = await Users.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        res.status(200).send({
+            status: 'Use successfully updated',
+            data: {
+                user,
+            },
+        });
+    } catch (err) {
+        res.status(404).send({
+            status: 'User could not be updated',
+            message: err,
+        });
+    }
+});
+// Message Routes
+
+app.listen(3001, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
